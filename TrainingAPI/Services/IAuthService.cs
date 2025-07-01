4bf4408910c13b9e1d4273c10b98e382ad1313f8
kashish284 <102645811+kashish284@users.noreply.github.com>
2025-07-01 13:01:22 +0530

    deleted api folders

--- TrainingAPI/Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TrainingAPI.Models;
-
-namespace TrainingAPI.Services
-{
-    public interface IAuthService
-    {
-        Task<User?> AuthenticateAsync(string email, string password, string role);
-    }
-}
